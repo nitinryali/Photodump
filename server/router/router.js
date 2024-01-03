9783f06b6b465abdaf65402271937fd23b0372c7
Ryali Nitin sai srinivas <85013482+nitinryali@users.noreply.github.com>
2024-01-03 18:46:59 +0530

    Add files via upload

---
+++ server/router/router.js
@@ -0,0 +1,10 @@
+const route = require('express').Router();
+const controller = require('../controller/controller');
+const store = require('../middleware/multer');
+
+// routes
+route.get('/', controller.home);
+route.post('/uploadmultiple', store.array('images', 12), controller.uploads);
+route.get('/delete/:id', controller.deleteImage); // New route for deleting images
+
+module.exports = route;
